a86ce5f96a97bf5a2febfea18ad8f4a872563b4b
Andrea Nuvoli <andrea.nuvoli@studio.unibo.it>
2023-11-19 15:24:47 +0100

    Exercise 83 done

---
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,141 @@
+package it.unibo.deathnote.impl;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote {
+
+    private static final byte ILLEGAL_CAUSE_TIMEOUT = 40;
+    private static final long ILLEGAL_DETAILS_TIMEOUT = 6000 + ILLEGAL_CAUSE_TIMEOUT;
+
+    private Map<String, Death> deathNote = new LinkedHashMap<>();
+    private long timeStartWriting;
+    private String lastName;
+
+    @Override
+    public String getRule(final int ruleNumber) {
+        if(ruleNumber > 0 && ruleNumber < RULES.size()) {
+            return RULES.get(ruleNumber - 1);
+        }
+        else {
+            throw new IllegalArgumentException("This rule doesn't exist");
+        }
+    }
+
+    @Override
+    public void writeName(String name) {
+        Objects.requireNonNull(name);
+        this.lastName = name;
+        this.timeStartWriting = System.currentTimeMillis();
+        this.deathNote.put(name, new Death());
+    }
+
+    @Override
+    public boolean writeDeathCause(final String cause) {
+        if(Objects.isNull(cause) || this.deathNote.isEmpty()) {
+            throw new IllegalStateException("The name has to be written before the cause of death");
+        }
+        
+        if((System.currentTimeMillis() - this.timeStartWriting) <= ILLEGAL_CAUSE_TIMEOUT) {
+            this.deathNote.get(this.lastName).setCause(cause);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        if(Objects.isNull(details) || this.deathNote.isEmpty()) {
+            throw new IllegalStateException("The name has to be written before the details of death");
+        }
+
+        if((System.currentTimeMillis() - this.timeStartWriting) <= ILLEGAL_DETAILS_TIMEOUT) {
+            this.deathNote.get(this.lastName).setDetails(details);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String getDeathCause(String name) {
+        return this.deathNote.get(name).cause;
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        return this.deathNote.get(name).details;
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        for(final String names : this.deathNote.keySet()) {
+            if(names == name) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public class Death {
+
+        private static final String DEFAULT_DEATH = "heart attack";
+
+        private String cause;
+        private String details;
+
+        public Death(String cause, String details) {
+            this.cause = cause;
+            this.details = details;
+        }
+
+        public Death() {
+            this(DEFAULT_DEATH, "");
+        }
+
+        public void setCause(final String cause){
+            this.cause = cause;
+        }
+    
+        public void setDetails(final String details){
+            this.details = details;
+        }
+
+        /*
+        public Death writeCause(final String cause) {
+            return System.currentTimeMillis() < this.timeOfDeath + TIMEOUT_CAUSE ? new Death(cause, this.details) : this;
+        }
+
+        public Death writeDetails(final String details) {
+            return System.currentTimeMillis() < this.timeOfDeath + TIMEOUT_DETAILS ? new Death(this.cause, details) : this;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof Death other)) {
+                return false;
+            }
+            return Objects.equals(cause, other.cause)
+                && Objects.equals(details, other.details)
+                && timeOfDeath == other.timeOfDeath;
+        }
+
+        @Override
+        public int hashCode() {
+            if (hash == 0) {
+                hash = Objects.hash(cause, details, timeOfDeath);
+            }
+            return hash;
+        }
+        */
+
+    }    
+
+}
--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,98 @@
 package it.unibo.deathnote;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.IllegalArgumentException;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.*;
+
 class TestDeathNote {
+
+   private static final int ILLEGAL_INDEX = 0; // try also with < 1 and > RULE.size()
+   private static final String MARIO = "Mario";
+   private static final String LUIGI = "Luigi";
+
+   private DeathNoteImpl deathNote;
+
+   @BeforeEach
+   public void setUp() {
+      this.deathNote = new DeathNoteImpl();
+   }
+
+   @Test
+   public void testIllegalRule() {
+      try {
+         this.deathNote.getRule(ILLEGAL_INDEX);
+         Assertions.fail();
+      } catch (IllegalArgumentException iAE) {
+         Assertions.assertEquals("This rule doesn't exist", iAE.getMessage());
+      }
+   }
+
+   @Test
+   public void testBlankOrNullRule() {
+      for(final String rule : DeathNote.RULES) {
+         assertNotNull(rule);
+         assertFalse(rule.isBlank());
+      }
+   }
+
+   @Test
+   public void testWritingHumans() {
+      Assertions.assertFalse(this.deathNote.isNameWritten(MARIO));
+      this.deathNote.writeName(MARIO);
+      Assertions.assertTrue(this.deathNote.isNameWritten(MARIO));
+      Assertions.assertFalse(this.deathNote.isNameWritten(LUIGI));
+      Assertions.assertFalse(this.deathNote.isNameWritten(""));
+   }
+
+   @Test
+   public void testCauseOfDeath() throws InterruptedException {
+
+      try {
+         this.deathNote.writeDeathCause("eating red poisonous mushrooms");
+         Assertions.fail();
+      } catch (IllegalStateException iSE) {
+         Assertions.assertEquals("The name has to be written before the cause of death", iSE.getMessage());
+      }
+
+      this.deathNote.writeName(MARIO);
+      assertEquals("heart attack", this.deathNote.getDeathCause(MARIO));
+
+      this.deathNote.writeName(LUIGI);
+      assertTrue(this.deathNote.writeDeathCause("karting accident"));
+      assertEquals("karting accident", this.deathNote.getDeathCause(LUIGI));
+
+      Thread.sleep(100);
+
+      Assertions.assertFalse(this.deathNote.writeDeathCause("drowning"));
+      Assertions.assertEquals("karting accident", this.deathNote.getDeathCause(LUIGI));      
+   }
+
+   @Test
+   public void testDetailsOfDeath() throws InterruptedException {
+
+      try {
+         this.deathNote.writeDetails("eating red poisonous mushrooms");
+         Assertions.fail();
+      } catch (IllegalStateException iSE) {
+         Assertions.assertEquals("The name has to be written before the details of death", iSE.getMessage());
+      }
+
+      this.deathNote.writeName(MARIO);
+      assertEquals("", this.deathNote.getDeathDetails(MARIO));
+      assertTrue(this.deathNote.writeDetails("ran for too long"));
+      assertEquals("ran for too long", this.deathNote.getDeathDetails(MARIO));
+      
+      this.deathNote.writeName(LUIGI);
+      Thread.sleep(6100);
+      Assertions.assertFalse(this.deathNote.writeDetails("drowning"));
+      Assertions.assertEquals("", this.deathNote.getDeathDetails(LUIGI));      
+   }
 
 }
